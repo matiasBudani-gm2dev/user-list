f7378b6b1d9a40314b18a53962a97d5034471821
Matias-Budani <matias.budani@gm2dev.com>
2025-09-18 13:14:43 -0300

    feat: add exercise 1, 2 and part of 3

---
+++ script.js
@@ -0,0 +1,149 @@
+const URL = "https://64fb193acb9c00518f7aa434.mockapi.io/api/v1/userList"
+const cards = document.querySelector(".cards")
+
+let indexUser;
+const showCards = async() =>{
+    const response = await fetch(URL)
+    const resData = await response.json()
+    indexUser = 0
+    resData.forEach(user => {
+        const card = document.createElement("div")
+        card.id = user.id
+        card.classList.add("card")
+        const userName = document.createElement("p")
+        userName.textContent = user.name
+        const userEmail = document.createElement("p")
+        userEmail.textContent = user.email
+        const userId = document.createElement("p")
+        userId.textContent = user.id
+        const userDelete = document.createElement(`button`)
+        userDelete.id = indexUser
+        userDelete.onclick = () => deleteUser(user.id)
+        userDelete.textContent = "DELETE"
+        const userEdit = document.createElement(`button`)
+        userEdit.id = indexUser
+        userEdit.onclick = () => editUser(user.id)
+        userEdit.textContent = "EDIT"
+
+
+        card.appendChild(userId)
+        card.appendChild(userName)
+        card.appendChild(userEmail)
+        card.appendChild(userDelete)
+        card.appendChild(userEdit)
+        cards.appendChild(card)
+
+        indexUser++
+    });
+}
+
+const deleteUser = (id)=>{
+    const allCards = cards.querySelectorAll(".card")
+    allCards.forEach(async (card)=>{
+        if(card.id === id){ 
+            const response = await fetch((URL + `/${id}`), {
+                method: "DELETE"
+            })
+            if(response.ok){
+                console.log("Se borro el usuario correctamente")
+                
+            }else{
+                console.log("Eu ta mal")
+            }
+        }
+    })
+}
+const createUser = async (event)=>{
+    
+    event.preventDefault()
+
+    const newUserName = document.getElementById("new-user-name").value
+    console.log(newUserName)
+
+    const newUserEmail = document.getElementById("new-user-email").value
+    console.log(newUserEmail)
+
+    const newUserCountry = document.getElementById("new-user-country").value
+    console.log(newUserCountry)
+
+    const newUserAvatar = document.getElementById("new-user-avatar").value
+    console.log(newUserAvatar)
+
+    const newUserRoles = []
+    if(newUserRoles.length !== 0){
+        newUserRoles.push(document.getElementById("new-user-roles").value)
+    }
+
+
+    const response = await fetch(URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body : JSON.stringify({
+            name: newUserName,
+            email: newUserEmail,
+            country: newUserCountry,
+            avatar: newUserAvatar,
+            roles : newUserRoles
+        })
+    })
+    if(response.ok){
+        console.log("Se agrego correctamente")
+    }else{
+        console.log("Eu ta todo mal")
+    }
+}
+
+const editUser = async ()=>{
+    event.preventDefault()
+
+    const datosAEditar = {
+    }
+
+    const editUserName = document.getElementById("edit-user-name").value
+    console.log(editUserName)
+
+    if(editUserName){
+        datosAEditar.push(editUserName)
+    }
+
+    const editUserEmail = document.getElementById("edit-user-email").value
+    console.log(editUserEmail)
+
+    if(editUserEmail){
+        datosAEditar.push(editUserEmail)
+    }
+
+    const editUserCountry = document.getElementById("edit-user-country").value
+    console.log(editUserCountry)
+
+    if(editUserCountry){
+        datosAEditar.push(editUserCountry)
+    }
+
+    const editUserAvatar = document.getElementById("edit-user-avatar").value
+    console.log(editUserAvatar)
+
+    if(editUserAvatar){
+        datosAEditar.push(editUserAvatar)
+    }
+
+    const editUserRoles = []
+    if(editUserRoles.length !== 0){
+        editUserRoles.push(document.getElementById("edit-user-roles").value)
+    }
+
+    if(editUserRoles.length === 0){
+        datosAEditar.push(editUserRoles)
+    }
+
+    const response = fetch(URL, {
+       method: "PATCH",
+       headers : {"Content-Type": "application/json"
+        },
+        body
+    })
+}
+
+showCards()
